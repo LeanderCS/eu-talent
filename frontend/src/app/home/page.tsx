1cc2774e9d7dab0a92a93ee704659f252308a859
Bagharia <hu.alexis@hotmail.com>
2025-03-20 15:58:32 +0100

    fake commit

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -11,11 +11,10 @@ const Home: React.FC = () => {
   const [isPopupVisible, setIsPopupVisible] = useState<boolean>(false);
 
   useEffect(() => {
-    // Afficher la notification après 10 secondes
+    //Afficher la notifcation après 10 secondes
     const timer = setTimeout(() => {
       setIsPopupVisible(true);
     }, 10000);
-
     return () => clearTimeout(timer);
   }, []);
 
@@ -23,17 +22,10 @@ const Home: React.FC = () => {
     <div className="relative flex flex-col items-center justify-center w-full h-screen bg-black">
       <Header />
 
-<<<<<<< HEAD
-      {/* Conteneur de la carte */}
       <div className="w-[393px] h-[750px] overflow-hidden">
         <EuropeMap isPopupVisible={isPopupVisible} />
-=======
-      <div className="w-[393px] h-[750px] overflow-hidden relative z-10">
-        <EuropeMap />
->>>>>>> 44d9480291317659205a2a4a14c9b7dd9f02e084
       </div>
 
-      {/* Bouton d'accès au chat */}
       <button
         onClick={() => router.push("../connecteMessage")}
         className="w-[393px] p-3 bg-blue-600 text-white text-lg font-bold cursor-pointer mb-5"
@@ -41,7 +33,6 @@ const Home: React.FC = () => {
         Accéder au Chat
       </button>
 
-      {/* Afficher la popup et son overlay flou */}
       {isPopupVisible && <NotificationPopup onClose={() => setIsPopupVisible(false)} />}
     </div>
   );
